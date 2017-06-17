59e7ad9062d6aa1af546898b3d7bb52232be936f
bryan2894 <bryan2894@gmail.com>
2017-06-16 21:09:23 -0400

    ls990: update device tree

--- omni_ls990.mk
+++ omni_ls990.mk
@@ -38,9 +38,6 @@ PRODUCT_COPY_FILES += \
     device/lge/ls990/postrecoveryboot.sh:recovery/root/sbin/postrecoveryboot.sh \
     device/lge/ls990/multirom/sign:recovery/root/res/sign
 
-PRODUCT_COPY_FILES += device/lge/ls990/fstab.g3:recovery/root/fstab.g3
-PRODUCT_COPY_FILES += device/lge/ls990/multirom/twrp.fstab:recovery/root/etc/twrp.fstab
-
 PRODUCT_NAME := omni_ls990
 PRODUCT_DEVICE := ls990
 PRODUCT_BRAND := LG
@@ -52,8 +49,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="g3_spr_us" \
     BUILD_FINGERPRINT="lge/g3_spr_us/g3:4.4.2/KVT49L.LS990ZV4/LS990ZV4.1403169216:user/release-keys" \
     PRIVATE_BUILD_DESC="g3_spr_us-user 4.4.2 KVT49L.LS990ZV4 LS990ZV4.1403169216 release-keys"
-
-# Kernel inline build
-#TARGET_KERNEL_CONFIG := ls990_defconfig
-#TARGET_VARIANT_CONFIG := ls990_defconfig
-#TARGET_SELINUX_CONFIG := ls990_defconfig
