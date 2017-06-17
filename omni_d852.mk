b81b95c6e25fac53e6218a03ec3ce1285d36e4b1
bryan2894 <bryan2894@gmail.com>
2017-06-16 21:08:22 -0400

    d852: update device tree

--- omni_d852.mk
+++ omni_d852.mk
@@ -38,9 +38,6 @@ PRODUCT_COPY_FILES += \
     device/lge/d852/postrecoveryboot.sh:recovery/root/sbin/postrecoveryboot.sh \
     device/lge/d852/multirom/sign:recovery/root/res/sign
 
-PRODUCT_COPY_FILES += device/lge/d852/fstab.g3:recovery/root/fstab.g3
-PRODUCT_COPY_FILES += device/lge/d852/multirom/twrp.fstab:recovery/root/etc/twrp.fstab
-
 PRODUCT_NAME := omni_d852
 PRODUCT_DEVICE := d852
 PRODUCT_BRAND := LG
@@ -52,8 +49,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="g3_bell_ca" \
     BUILD_FINGERPRINT="lge/g3_global_com/g3:5.0/LRX21R.A1421650137/1421650137:user/release-keys" \
     PRIVATE_BUILD_DESC="g3_global_com-user 5.0 LRX21R.A1421650137 1421650137 release-keys"
-
-# Kernel inline build
-#TARGET_KERNEL_CONFIG := d852_defconfig
-#TARGET_VARIANT_CONFIG := d852_defconfig
-#TARGET_SELINUX_CONFIG := d852_defconfig
