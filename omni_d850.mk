3435ccc9eeb74de455ff0ee15378be266319da95
bryan2894 <bryan2894@gmail.com>
2017-06-16 21:07:59 -0400

    d850: update device tree

--- omni_d850.mk
+++ omni_d850.mk
@@ -38,8 +38,6 @@ PRODUCT_COPY_FILES += \
     device/lge/d850/postrecoveryboot.sh:recovery/root/sbin/postrecoveryboot.sh \
     device/lge/d850/multirom/sign:recovery/root/res/sign
 
-PRODUCT_COPY_FILES += device/lge/d850/fstab.g3:recovery/root/fstab.g3
-PRODUCT_COPY_FILES += device/lge/d850/multirom/twrp.fstab:recovery/root/etc/twrp.fstab
 PRODUCT_NAME := omni_d850
 PRODUCT_DEVICE := d850
 PRODUCT_BRAND := LG
@@ -51,8 +49,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="g3_att_us" \
     BUILD_FINGERPRINT="lge/g3_att_us/g3:4.4.2/KVT49L.D85010d/D85010d.1403173065:user/release-keys" \
     PRIVATE_BUILD_DESC="g3_att_us-user 4.4.2 KVT49L.D85010d D85010d.1403173065 release-keys"
-
-# Kernel inline build
-#TARGET_KERNEL_CONFIG := d850_defconfig
-#TARGET_VARIANT_CONFIG := d850_defconfig
-#TARGET_SELINUX_CONFIG := d850_defconfig
